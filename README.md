8a265d4f56bb26c73040f8193d12c1265427611e
sabik <ivan.rodriguez.o@gmail.com>
2014-07-08 13:56:08 -0500

    agregados csvs, python con csv por poliza sin pandas

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ opensource-aseguradoras
 =======================
 
 Pandas, Mongodb y Hadoop para cálculos y análisis de aseguradoras
+
+- Pandas para cálculos actuariales y estadísticas
+- Mongodb para API
+- Hadoop para análisis y estadísticas con volumen de datos grande
--- calculos-actuariales/ejemplo_pnnx.py
+++ calculos-actuariales/ejemplo_pnnx.py
@@ -2,21 +2,30 @@
 
 import csv
 from calculadora_pnnx import pnnx
+import ast
+import time
 
+tiempo_inicio = time.time()
 f = open('tabla_mortalidad.csv')
 reader = csv.reader(f)
 tabla_mortalidad = {}
 reader.next()
 for row in reader:
-    tabla_mortalidad[int(row[0])] = float(row[1])
-
+    tabla_mortalidad[ast.literal_eval(row[0])] = ast.literal_eval(row[1])
 f = open('polizas.csv')
 reader = csv.reader(f)
-tabla_mortalidad = {}
 reader.next()
+
+calculos_pnnx = {}
 for row in reader:
-    edad = int(row[0])
-    sa = float(row[1])
-    plazo = int(row[2])
-    moneda = row[3]
-    print pnnx(edad, sa, plazo, moneda, tabla_mortalidad)
+    num_poliza = row[0]
+    moneda = row[2]
+    tipo_plan = row[3]
+    edad = ast.literal_eval(row[4])
+    sa = ast.literal_eval(row[5])
+    plazo = 5
+    calculo_pnnx = pnnx(edad, sa, plazo, moneda, tabla_mortalidad)
+    calculos_pnnx[num_poliza] = calculo_pnnx
+    print 'Póliza número ' + num_poliza + ', PNNx: ' + str(calculo_pnnx)
+print '----------'
+print 'Tiempo: ' + str(time.time() - tiempo_inicio) + ' segs.'
