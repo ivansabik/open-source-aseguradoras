4a88cb73e53663800b9d18e907dc7f52b8b4c328
sabik <ivan.rodriguez.o@gmail.com>
2014-07-08 19:35:37 -0500

    df a .astype('float')

--- calculos-actuariales/ejemplo_pnnx_pandas.py
+++ calculos-actuariales/ejemplo_pnnx_pandas.py
@@ -13,14 +13,11 @@ print '---------- Tabla Mortalidad ----------'
 df_tabla_mortalidad = pd.read_csv('tabla_mortalidad.csv')
 df_tabla_mortalidad['px'] = 1-df_tabla_mortalidad['qx']
 print df_tabla_mortalidad.head()
-print df_tabla_mortalidad.describe()
 
 print '---------- Tabla Vt ----------'
-df_tabla_vt = pd.DataFrame({'Vt': range(0,100), 't': range(0,100)})
+df_tabla_vt = pd.DataFrame({'Vt': range(0,100), 't': range(0,100)}).astype('float')
 def Vt(df_tabla_vt):
-    df_tabla_vt['Vt'] = float(pow(i+1, -df_tabla_vt['t']))
+    df_tabla_vt['Vt'] = pow(i+1, -df_tabla_vt['t'])
     return df_tabla_vt
 df_tabla_vt.apply(Vt, axis=1)
-pd.set_printoptions(precision=10)
 print df_tabla_vt.head()
-print df_tabla_vt.describe()
